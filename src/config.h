da912ada4463f2dddba4dec137307cabffee4adb
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-31 09:24:34 +0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,36 +1,36 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#ifndef PISERVER_DATADIR
-#define PISERVER_DATADIR  "/var/lib/piserver"
+#ifndef EFSERVER_DATADIR
+#define EFSERVER_DATADIR  "/var/lib/efserver"
 #endif
 
-#define PACKAGE            "piserver"
+#define PACKAGE            "efserver"
 
-#define PISERVER_HOSTSFILE  PISERVER_DATADIR "/hosts.json"
-#define PISERVER_DISTROFILE PISERVER_DATADIR "/installed_distros.json"
-#define PISERVER_SETTINGSFILE PISERVER_DATADIR "/settings.json"
-#define PISERVER_TFTPROOT   PISERVER_DATADIR "/tftproot"
-#define PISERVER_DISTROROOT PISERVER_DATADIR "/os"
-#define PISERVER_SHAREDROOT PISERVER_DISTROROOT "/shared"
-#define PISERVER_HOMEROOT   "/home"
-#define PISERVER_DNSMASQCONFFILE "/etc/dnsmasq.d/piserver"
-#define PISERVER_REPO_URL  "http://downloads.raspberrypi.org/piserver.json"
-#define PISERVER_REPO_CACHEFILE PISERVER_DATADIR "/avail_distros_cache.json"
-#define PISERVER_POSTINSTALLSCRIPT PISERVER_DATADIR "/scripts/postinstall.sh"
+#define EFSERVER_HOSTSFILE  EFSERVER_DATADIR "/hosts.json"
+#define EFSERVER_DISTROFILE EFSERVER_DATADIR "/installed_distros.json"
+#define EFSERVER_SETTINGSFILE EFSERVER_DATADIR "/settings.json"
+#define EFSERVER_TFTPROOT   EFSERVER_DATADIR "/tftproot"
+#define EFSERVER_DISTROROOT EFSERVER_DATADIR "/os"
+#define EFSERVER_SHAREDROOT EFSERVER_DISTROROOT "/shared"
+#define EFSERVER_HOMEROOT   "/home"
+#define EFSERVER_DNSMASQCONFFILE "/etc/dnsmasq.d/Efserver"
+#define EFSERVER_REPO_URL  "https://github.com/efscoin/efserver.json"
+#define EFSERVER_REPO_CACHEFILE EFSERVER_DATADIR "/avail_distros_cache.json"
+#define EFSERVER_POSTINSTALLSCRIPT EFSERVER_DATADIR "/scripts/postinstall.sh"
 
 #define OUI_FILTER         "^(b8:27:eb|dc:a6:32):"
 #define DHCPANALYZER_FILTER_STRING "PXEClient"
 
-#define PISERVER_LDAP_URL  "ldapi:///"
-#define PISERVER_DOMAIN    "raspberrypi.local"
-#define PISERVER_LDAP_DN   "dc=raspberrypi,dc=local"
+#define EFSERVER_LDAP_URL  "ldapi:///"
+#define EFSERVER_DOMAIN    "efscoin.local"
+#define EFSERVER_LDAP_DN   "dc=efscoin,dc=local"
 
-#define PISERVER_LDAP_USER "cn=admin,dc=raspberrypi,dc=local"
+#define EFSERVER_LDAP_USER "cn=admin,dc=efscoin,dc=local"
 
-#define PISERVER_LDAP_TIMEOUT  10
-#define PISERVER_MIN_UID   10000
-#define PISERVER_GID       100
-#define PISERVER_SHELL     "/bin/bash"
+#define EFSERVER_LDAP_TIMEOUT  10
+#define EFSERVER_MIN_UID   10000
+#define EFSERVER_GID       100
+#define EFSERVER_SHELL     "/bin/bash"
 
 #endif // CONFIG_H
